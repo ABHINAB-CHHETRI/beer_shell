7192bc257e4194570f38c51f3159cf05e9c8e224
ABHINAB-CHHETRI <achheri_be23@thapar.edu>
2025-10-02 09:50:13 +0530

    first commit

---
+++ sys_fetch_module.lua
@@ -0,0 +1,16 @@
+local M = {}   -- make a table that will hold your module's functions
+--popen (pipeopen) allows to execute external system commands
+--read("*l") reads first line , *a reads all 
+
+-- ANSI color codes
+local YELLOW = "\27[33m"
+local RESET = "\27[0m"
+
+function M.sysfetch()
+    local os_name = io.popen("uname -s"):read("*l") or "Unknown"
+    local hostname = io.popen("hostname"):read("*l") or "Unknown"
+    local uptime = io.popen("uptime -p"):read("*l") or "Unknown"
+
+    io.write(YELLOW .."Operating System:", os_name, "\nHostname:", hostname, "\nUptime:", uptime.. RESET) 
+end
+return M  -- return the table so other files can use it
